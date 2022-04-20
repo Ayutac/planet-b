3756e2a7772e906fc565b58fd16129a52ffd1103
Ayutac <skoch02@students.uni-mainz.de>
2022-04-20 09:24:39 +0200

    Added the RIM Casing

--- src/main/java/org/abos/fabricmc/planetb/Content.java
+++ src/main/java/org/abos/fabricmc/planetb/Content.java
@@ -1,9 +1,29 @@
 package org.abos.fabricmc.planetb;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class Content {
 
-    public static void init() {
+    public static final String REINFORCED_INDUSTRIAL_MACHINE_CASING_STR = "reinforced_industrial_machine_casing";
+    public static final Block REINFORCED_INDUSTRIAL_MACHINE_CASING = new Block(FabricBlockSettings.of(Material.METAL, MapColor.GRAY).strength(10f, 100f).sounds(BlockSoundGroup.NETHERITE));
 
+    public static void init() {
+        initBlocks();
+    }
+
+    public static void initBlocks() {
+        Registry.register(Registry.BLOCK, new Identifier(PlanetB.MOD_ID, REINFORCED_INDUSTRIAL_MACHINE_CASING_STR), REINFORCED_INDUSTRIAL_MACHINE_CASING);
+        Registry.register(Registry.ITEM, new Identifier(PlanetB.MOD_ID, REINFORCED_INDUSTRIAL_MACHINE_CASING_STR), new BlockItem(REINFORCED_INDUSTRIAL_MACHINE_CASING, new FabricItemSettings().group(ItemGroup.MISC)));
     }
 
     private Content() {/* No instantiation. */}

b057c13d2faa83456a15c58f81b0cd80651cb26b
Ayutac <skoch02@students.uni-mainz.de>
2022-04-20 09:27:24 +0200

    Added TerraBlender entry point

--- src/main/java/org/abos/fabricmc/planetb/PlanetB.java
+++ src/main/java/org/abos/fabricmc/planetb/PlanetB.java
@@ -3,8 +3,9 @@ package org.abos.fabricmc.planetb;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import terrablender.api.TerraBlenderApi;
 
-public class PlanetB implements ModInitializer {
+public class PlanetB implements ModInitializer, TerraBlenderApi {
 
 	public static final String MOD_ID = "planet-b";
 
@@ -15,5 +16,10 @@ public class PlanetB implements ModInitializer {
 		LOGGER.info("Initializing...");
 		Content.init();
 		LOGGER.info("Initializing done! Good travels!");
+	}
+
+	@Override
+	public void onTerraBlenderInitialized() {
+
 	}
 }

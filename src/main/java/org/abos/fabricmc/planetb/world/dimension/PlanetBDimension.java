3f72a6f27eb5445a53021daacda09f70b2ba4d1c
Ayutac <skoch02@students.uni-mainz.de>
2022-04-21 04:19:34 +0200

    Added custom portal API + impl.

--- src/main/java/org/abos/fabricmc/planetb/world/dimension/PlanetBDimension.java
+++ src/main/java/org/abos/fabricmc/planetb/world/dimension/PlanetBDimension.java
@@ -1,19 +1,21 @@
 package org.abos.fabricmc.planetb.world.dimension;
 
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
+import org.abos.fabricmc.planetb.Content;
 import org.abos.fabricmc.planetb.PlanetB;
+import techreborn.init.TRContent;
 
 public class PlanetBDimension {
 
-    public static final RegistryKey<DimensionOptions> DIMENSION_KEY = RegistryKey.of(
-            Registry.DIMENSION_KEY,
-            new Identifier(PlanetB.MOD_ID, PlanetB.MOD_ID)
-    );
+    public static final Identifier DIMENSION_ID = new Identifier(PlanetB.MOD_ID, PlanetB.MOD_ID);
+
+    public static final RegistryKey<DimensionOptions> DIMENSION_KEY = RegistryKey.of(Registry.DIMENSION_KEY, DIMENSION_ID);
 
     public static final RegistryKey<World> WORLD_KEY = RegistryKey.of(
             Registry.WORLD_KEY,
@@ -26,7 +28,14 @@ public class PlanetBDimension {
     );
 
     public static void init() {
-        PlanetB.LOGGER.info("Registering dimension for + " + PlanetB.MOD_ID);
+        PlanetB.LOGGER.info("Initializing dimension for + " + PlanetB.MOD_ID);
+        CustomPortalBuilder.beginPortal()
+                .frameBlock(Content.REINFORCED_INDUSTRIAL_MACHINE_CASING)
+                .lightWithItem(TRContent.WRENCH)
+                .onlyLightInOverworld()
+                .destDimID(DIMENSION_ID)
+                .tintColor(0x99c0cc)
+                .registerPortal();
     }
 
     private PlanetBDimension() {/* No instantiation. */}

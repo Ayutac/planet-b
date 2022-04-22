cce058a149197461c151ce5f999510e197355740
Ayutac <skoch02@students.uni-mainz.de>
2022-04-22 22:20:57 +0200

    Added datagen hook

---
+++ src/main/java/org/abos/fabricmc/planetb/datagen/PlanetBDataGeneratorEntry.java
@@ -0,0 +1,11 @@
+package org.abos.fabricmc.planetb.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlanetBDataGeneratorEntry implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    }
+}

6513ffb5e452089dd7563a56adf300b0571858a5
Ayutac <skoch02@students.uni-mainz.de>
2022-04-22 23:13:27 +0200

    Added block loot table but it doesn't work

--- src/main/java/org/abos/fabricmc/planetb/datagen/PlanetBDataGeneratorEntry.java
+++ src/main/java/org/abos/fabricmc/planetb/datagen/PlanetBDataGeneratorEntry.java
@@ -2,10 +2,13 @@ package org.abos.fabricmc.planetb.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.abos.fabricmc.planetb.datagen.providers.BlockLootTableProvider;
 
 public class PlanetBDataGeneratorEntry implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.addProvider(BlockLootTableProvider::new);
+
     }
 }
---
+++ src/main/java/org/abos/fabricmc/planetb/datagen/providers/BlockLootTableProvider.java
@@ -0,0 +1,17 @@
+package org.abos.fabricmc.planetb.datagen.providers;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.data.server.BlockLootTableGenerator;
+import org.abos.fabricmc.planetb.Content;
+
+public class BlockLootTableProvider extends FabricBlockLootTableProvider {
+    public BlockLootTableProvider(FabricDataGenerator dataGenerator) {
+        super(dataGenerator);
+    }
+
+    @Override
+    protected void generateBlockLootTables() {
+        addDrop(Content.PORTAL_FRAME);
+    }
+}

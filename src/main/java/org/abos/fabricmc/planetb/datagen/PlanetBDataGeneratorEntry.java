0a70dabd2b48049a623b15f0c05263782575515c
Ayutac <skoch02@students.uni-mainz.de>
2022-04-26 07:43:31 +0200

    Removed unnecessary line of code

--- src/main/java/org/abos/fabricmc/planetb/datagen/PlanetBDataGeneratorEntry.java
+++ src/main/java/org/abos/fabricmc/planetb/datagen/PlanetBDataGeneratorEntry.java
@@ -12,7 +12,6 @@ public class PlanetBDataGeneratorEntry implements DataGeneratorEntrypoint {
         fabricDataGenerator.addProvider(BlockTagProvider::new);
         fabricDataGenerator.addProvider(ItemTagProvider::new);
         // tags always first!
-        Content.getItemGroup(); // make sure item group is populated
         fabricDataGenerator.addProvider(RecipeProvider::new);
         fabricDataGenerator.addProvider(BlockLootTableProvider::new);
         fabricDataGenerator.addProvider(ModelProvider::new);
